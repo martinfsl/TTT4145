ba8f3b0e9ff23a05fa0ebe13c8f994ff0459cb6c
martinfsl <74840025+martinfsl@users.noreply.github.com>
2024-01-23 09:54:39 +0100

    Added real-time spectrum analyzer

---
+++ Setup/receiveAndPlotSpectrum.m
@@ -0,0 +1,20 @@
+% Create a Spectrum Analyzer System object
+spectrumAnalyzer = dsp.SpectrumAnalyzer(...
+    'SampleRate', rx.BasebandSampleRate, ...
+    'SpectralAverages', 10, ...
+    'YLimits', [-130, -30], ...
+    'Title', 'Real-Time Spectrum of Received Signal', ...
+    'FrequencySpan', 'Span and center frequency');
+
+% Stream processing loop
+for frame = 1:numSamples
+    % Receive signal from Pluto SDR
+    rxSig = rx();
+    
+    % Plot spectrum
+    spectrumAnalyzer(rxSig);
+end
+
+% Release the System objects
+release(rx);
+release(spectrumAnalyzer);
