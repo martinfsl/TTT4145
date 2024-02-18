94c3c373bc8a1670dcbdacb0efd63b9b3a15a252
vebroo <vebjorn.rooth@gmail.com>
2024-02-18 19:48:21 +0100

    Added measurements script

---
+++ Measurements/measurements.m
@@ -0,0 +1,28 @@
+close all; clear; clc;
+
+fileName = "Data\signal-RBW-100kHz-new.csv";
+file = readmatrix(fileName); 
+
+% hard coded numbers for formatting (same file format each time)
+data = file(4:end,1:2);
+xAxis = data(144:end,2); % frequency axis (arbitrary, but uses 1.79 GHz - 1.81 GHz)
+yAxis = data(144:end,1); % avg VRMS power [dBm]
+
+% power calculations
+attenuator = 30.15; % cable + attenuator loss in dB
+indices = 369:436; % indices ~ 369:431 (signal) 395:412 (carrier) % when using 801 points
+totalSignalPower = 10*log10(sum( 10.^(yAxis(indices)/10) ))
+outputSignalPower = totalSignalPower + attenuator
+
+indicesPlot = 300:500;
+stem(xAxis(indicesPlot), yAxis(indicesPlot)); hold on;
+stem(xAxis(indices), yAxis(indices), 'r')
+
+% noise calculations
+% k = 1.38e-23;
+% bandwidth = xAxis(end)-xAxis(1);
+
+% totalNoisePower_abs = sum( 10.^(yAxis(:)/10) );
+% totalNoisePower = 10*log10(totalNoisePower_abs)
+% antennaTemperature = totalNoisePower_abs/(k*bandwidth)
+% antennaNoiseDensity = 10*log10((10^(yAxis(1)/10) + 10^(yAxis(end)/10))/(2*bandwidth))
