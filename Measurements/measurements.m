4e7f196f194540e714d91728d2f7e6efb6b83a5d
vebroo <vebjorn.rooth@gmail.com>
2024-02-18 21:22:05 +0100

    added polarization loss calculation

--- Measurements/measurements.m
+++ Measurements/measurements.m
@@ -1,6 +1,6 @@
 close all; clear; clc;
 
-fileName = "Data\carrier-RBW-100kHz-new.csv";
+fileName = "Data\polarization_LOS-3.csv";
 file = readmatrix(fileName); 
 
 % hard coded numbers for formatting (same file format each time)
@@ -10,19 +10,26 @@ yAxis = data(144:end,1); % avg VRMS power [dBm]
 
 % power calculations
 attenuator = 30.15; % cable + attenuator loss in dB
-indices = 396:413; % indices ~ 369:431 (signal), ~ 395:412 (carrier) % when using 801 points
-totalSignalPower = 10*log10(sum( 10.^(yAxis(indices)/10) ))
-outputSignalPower = totalSignalPower + attenuator
+indices = 398:411; % indices ~ 369:431 (signal), ~ 395:412 (carrier) % when using 801 points
+receivedSignalPower = 10*log10(sum( 10.^(yAxis(indices)/10) ))
+
+% transmit power
+transmitSignalPower = receivedSignalPower + attenuator
+
+% polarization loss
+FSL = 55; % 7.5 m, 1.8 GHz
+transmitPower = 4.5; % dBm (using measured data)
+polarizationLoss = transmitPower - FSL - receivedSignalPower 
 
 indicesPlot = 300:500;
 stem(xAxis(indicesPlot), yAxis(indicesPlot)); hold on;
 stem(xAxis(indices), yAxis(indices), 'r')
 
 % noise calculations
-% k = 1.38e-23;
-% bandwidth = xAxis(end)-xAxis(1);
+k = 1.38e-23;
+bandwidth = xAxis(end)-xAxis(1);
 
-% totalNoisePower_abs = sum( 10.^(yAxis(:)/10) );
+totalNoisePower_abs = sum( 10.^(yAxis(:)/10) );
 % totalNoisePower = 10*log10(totalNoisePower_abs)
 % antennaTemperature = totalNoisePower_abs/(k*bandwidth)
 % antennaNoiseDensity = 10*log10((10^(yAxis(1)/10) + 10^(yAxis(end)/10))/(2*bandwidth))
