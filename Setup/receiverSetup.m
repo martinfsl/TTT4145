2563661fc31f44f5918708cf7c0ddf510279e948
martinfsl <74840025+martinfsl@users.noreply.github.com>
2024-02-23 12:52:18 +0100

    Clean-up

--- Setup/receiverSetup.m
+++
@@ -1,9 +0,0 @@
-% Set up the receiver
-% Use the default value of 0 for FrequencyCorrection, which corresponds to 
-% the factory-calibrated condition
-rx = sdrrx('Pluto', 'RadioID', 'usb:0', 'CenterFrequency', centerFreq, ...
-           'BasebandSampleRate', sampleRate, 'SamplesPerFrame', numSamples, ...
-           'OutputDataType', 'double', 'ShowAdvancedProperties', true);
-       
-% Use the info method to show the actual values of various hardware-related properties
-rxRadioInfo = info(rx)
