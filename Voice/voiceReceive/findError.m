5251de6d9d962deaa1b2d72e55c4aaae85b5426f
martinfsl <74840025+martinfsl@users.noreply.github.com>
2024-02-25 14:35:18 +0100

    Now shows the errors in each packet

--- Voice/voiceReceive/findError.m
+++ Voice/voiceReceive/findError.m
@@ -8,4 +8,13 @@ cd(Current_Dir);
 error = sum(voiceMessage ~= trueMessage);
 error_Pb = error/length(voiceMessage);
 
-errorDiff = (voiceMessage ~= trueMessage);
+% errorDiff = (voiceMessage ~= trueMessage);
+errorDiff = zeros(length(allReceivedHeaders), 1);
+
+for i = 1:length(allReceivedHeaders)
+    startIndex = (i-1)*(2*frameSize - 1) + 1;
+    endIndex = i*(2*frameSize);
+    errorDiff(i, 1) = sum(...
+        voiceMessage(startIndex:endIndex) ~= ...
+        trueMessage(startIndex:endIndex));
+end
