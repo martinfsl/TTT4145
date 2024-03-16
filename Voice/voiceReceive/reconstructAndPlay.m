a9335b1455b3306f0da93db89b6891ab710a7cc4
vebroo <vebjorn.rooth@gmail.com>
2024-03-16 13:41:21 +0100

    hjem

--- Voice/voiceReceive/reconstructAndPlay.m
+++ Voice/voiceReceive/reconstructAndPlay.m
@@ -1,8 +1,9 @@
-receivedMessages = [];
-for i = 1:size(allMessages, 2)
-    receivedMessages = [receivedMessages; allMessages(:, i)];
-end
+% receivedMessages = [];
+% for i = 1:size(allMessages, 2)
+%     receivedMessages = [receivedMessages; allMessages(:, i)];
+% end
 
-recVoice = reconstructVoiceSignal(receivedMessages);
+voiceMessagesCut = voiceMessages(:, 1:64);
+recVoice = reconstructVoiceSignal(voiceMessagesCut(:));
 
 sound(recVoice, 16000);
